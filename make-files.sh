63550e9620b6c111d6ed8a4096e7ea69023c1724
Christian Starkjohann <cs+github@obdev.at>
2008-04-17 19:30:20 +0000

    - added make-files.sh script

---
+++ make-files.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Author: Christian Starkjohann
+# Creation Date: 2008-04-17
+# Tabsize: 4
+# Copyright: (c) 2008 by OBJECTIVE DEVELOPMENT Software GmbH
+# License: GNU GPL v2 (see License.txt) or proprietary (CommercialLicense.txt)
+# This Revision: $Id$
+
+
+find . -mindepth 2 -name 'make-files.sh' -exec sh -c "cd \`dirname {}\`; ./make-files.sh \"$1\"" \;
