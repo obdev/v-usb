42a85e6e30fc32916e75bf609ebb72671efa0cb8
Christian Starkjohann <cs+github@obdev.at>
2009-03-16 18:51:56 +0000

    - extended tests to 18 MHz module with CRC

--- tests/usbconfig.h
+++ tests/usbconfig.h
@@ -47,6 +47,13 @@ section at the end of this file).
  * of 2000 ppm and thus a crystal!
  * Default if not specified: 12 MHz
  */
+#define USB_CFG_CHECK_CRC       (USB_CFG_CLOCK_KHZ == 18000)
+/* Define this to 1 if you want that the driver checks data integrity of data
+ * packets (CRC checks). CRC checks cost quite a bit of code size and are
+ * currently only available for 18 MHz crystal clock. You must choose
+ * USB_CFG_CLOCK_KHZ = 18000 if you enable this option.
+ */
+
 
 /* ----------------------- Optional Hardware Config ------------------------ */
 
